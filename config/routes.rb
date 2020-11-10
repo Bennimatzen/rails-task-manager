112d2f7019fd842d6bf6401ac2c36aecc0ff1e06
Benni Matzen <benniafmatzen@web.de>
2020-11-10 16:48:42 +0100

    nwkwn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # Read all
+  get "tasks", to: 'tasks#index'
+  # Create a new restaurant
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Update a restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # # Delete a restaurant
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
 end
